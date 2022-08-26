e3873020182c43ff8bb87ad7254661b6fcb82cb5
UgurDay <ugur.begenmis24@gmail.com>
2022-08-26 14:18:43 +0300

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,50 @@
+//import
+//main function
+//calling of main function
+
+const { networks } = require("../hardhat.config")
+
+//function deployFunc() {
+// console.log("Hi!")
+//}
+//module.exports.default = deployFunc
+
+//module.exports = async (hre) => {
+//const { getNamedAccounts, deployments } = hre
+
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+
+  //const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+  let ethUsdPriceFeedAddress
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+    ethUsdPriceFeedAddress = ethUsdAggregator.address
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+  }
+
+  // if the contract doesn't exist, we deploy a minimal version of
+  // for our local testing
+  const args = [ethUsdPriceFeedAddress]
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: args, //put price feed addy
+    log: true,
+  })
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args)
+  }
+  log("----------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
